399abe61528c79fc2d5d41706079c9a37242a8b0
Tilman Kurmayer <tilman.kurmayer@gmail.com>
2025-08-25 10:33:06 +0200

    feat: :sparkles: Increase buffer size to 4096 for improved efficiency in cat.asm

--- cat.asm
+++ cat.asm
@@ -1,5 +1,5 @@
 section .data
-    buffer_size equ 256
+    buffer_size equ 4096 ; Increased buffer size for efficiency
 
 section .bss
     read_buffer resb buffer_size
@@ -7,44 +7,66 @@ section .bss
 section .text
     global _start
 _start:
-
+    ; Check command line arguments to get the filename
     mov rdx, [rsp + 16]
     xor rcx, rcx
 
-    .count_loop:
-        cmp byte [rdx + rcx], 0
-        je .open_file
-        inc rcx
-        jmp .count_loop
-    
-    .open_file:
-        mov rax, 2
-        mov rdi, [rsp + 16]
-        mov rsi, 0
-        mov rdx, 0
-        syscall
+.count_loop:
+    cmp byte [rdx + rcx], 0
+    je .open_file
+    inc rcx
+    jmp .count_loop
 
-        mov r12, rax
+.open_file:
+    ; Syscall for opening a file
+    mov rax, 2               ; syscall number for open
+    mov rdi, [rsp + 16]      ; filename is the first argument
+    mov rsi, 0               ; read-only flags
+    mov rdx, 0               ; mode is ignored for read-only
+    syscall
 
-    .read_file:
-        mov rax, 0
-        mov rdi, r12
-        mov rsi, read_buffer
-        mov rdx, buffer_size
-        syscall
-    
-    .print_file:
+    mov r12, rax             ; Store the file descriptor in r12
 
-        mov rbx, rax
+    ; Check for open error
+    cmp r12, 0
+    jl .exit_error           ; If r12 < 0, it's an error code
 
-        mov rax, 1
-        mov rdi, 1
-        mov rsi, read_buffer
-        mov rdx, rbx
-        syscall
+.read_file_loop:
+    ; Syscall for reading from the file
+    mov rax, 0               ; syscall number for read
+    mov rdi, r12             ; file descriptor
+    mov rsi, read_buffer     ; buffer to read into
+    mov rdx, buffer_size     ; number of bytes to read
+    syscall
 
-    .exit_file:
+    mov rbx, rax             ; Store the number of bytes read in rbx
 
-        mov rax, 60
-        mov rdi, 0
-        syscall
+    ; Check if read returned 0 (end of file)
+    cmp rbx, 0
+    je .close_file           ; If rbx is 0, we've reached the end of the file
+
+    ; Syscall to print to standard output
+    mov rax, 1               ; syscall number for write
+    mov rdi, 1               ; file descriptor for stdout
+    mov rsi, read_buffer     ; buffer to write from
+    mov rdx, rbx             ; number of bytes to write (what we just read)
+    syscall
+
+    jmp .read_file_loop      ; Loop back to read more data
+
+.close_file:
+    ; Syscall for closing the file
+    mov rax, 3               ; syscall number for close
+    mov rdi, r12             ; file descriptor
+    syscall
+
+.exit:
+    ; Syscall to exit the program
+    mov rax, 60              ; syscall number for exit
+    mov rdi, 0               ; exit status
+    syscall
+
+.exit_error:
+    mov rax, 60              ; syscall number for exit
+    mov rdi, 1               ; exit status (error)
+    syscall
