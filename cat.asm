298b871de7b3fa848e72b389d048340bc87b5222
Tilman Kurmayer <tilman.kurmayer@gmail.com>
2025-08-25 08:55:03 +0200

    feat: :sparkles: Update cat.asm to read from README.md and output its content

--- cat.asm
+++ cat.asm
@@ -1,15 +1,35 @@
 section .data
-    msg db "Hello World", 0xA
-    len equ $ - msg
+    file_path db "README.md", 0
+    buffer_size equ 256
+
+section .bss
+    read_buffer resb buffer_size
 
 section .text
     global _start
 _start:
+    mov rax, 2
+    mov rdi, file_path
+    mov rsi, 0
+    mov rdx, 0
+    syscall
+
+    mov r12, rax
+
+    mov rax, 0
+    mov rdi, r12
+    mov rsi, read_buffer
+    mov rdx, buffer_size
+    syscall
+
+    mov rbx, rax
+
     mov rax, 1
     mov rdi, 1
-    mov rsi, msg
-    mov rdx, len
+    mov rsi, read_buffer
+    mov rdx, rbx
     syscall
+
 
     mov rax, 60
     mov rdi, 0
